c69a74c2cc0f984462d1734a8cdf9e9145cafa81
Jatin-tec <jatin.kshatriya2821@gmail.com>
2021-10-07 13:53:06 +0530

    nQueen Working

--- nQueen.py
+++ nQueen.py
@@ -1,5 +1,5 @@
 def drawBoard():
-    board = [[0]*4]*4
+    board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
     return board
 
 def isAttacked(board, m, n):
@@ -12,26 +12,39 @@ def isAttacked(board, m, n):
 
     for queenPosition in queenPositions:
         if (queenPosition[0]+queenPosition[1] == m+n or queenPosition[0]-queenPosition[1] == m-n):
+            # print('digonal')
             return True
         elif (queenPosition[0]==m or queenPosition[1]==n):
+            # print(queenPosition)
+            # print('row_col')
             return True
     
     return False    
 
 def placeQueen(board, qpos):
-    if qpos==1:
+    if qpos>=4:
         return True
  
     for i in range(len(board)):      
         if not isAttacked(board, i, qpos):
             board[i][qpos] = 'Q'
 
-        if(placeQueen(board, qpos+1)==True):
-            return True
-
-        board[i][qpos]=0     
+            if(placeQueen(board, qpos+1)==True):
+                return True
+            
+            board[i][qpos]=0     
+    
     return False
 
-board = drawBoard()
-placeQueen(board, 0)
-print(board)    
+
+if __name__ == '__main__':
+    board = drawBoard()
+
+    placeQueen(board, 0)
+
+    for row in range(len(board)):
+        for col in range(len(board)):
+            print(board[row][col], end=' ')   
+        print('\n')   
+
+    print(isAttacked(board, 2, 3))     
